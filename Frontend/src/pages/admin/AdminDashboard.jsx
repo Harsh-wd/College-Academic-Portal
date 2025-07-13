2aab7362666619286d472954d5091592693c6233
Harsh-wd <harshbansal.it26@gmail.com>
2025-07-13 22:52:35 +0530

    admin files  added

---
+++ Frontend/src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,182 @@
+import { useState } from 'react';
+import {
+    CssBaseline,
+    Box,
+    Toolbar,
+    List,
+    Typography,
+    Divider,
+    IconButton,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AppBar, Drawer } from '../../components/styles.js';
+import Logout from '../Logout.jsx';
+import SideBar from './SideBar.jsx';
+import AdminProfile from './AdminProfile.jsx';
+import AdminHomePage from './AdminHomePage.jsx';
+
+import AddStudent from './studentRelated/AddStudent.jsx';
+
+import ShowStudents from './studentRelated/ShowStudents.jsx';
+import StudentAttendance from './studentRelated/StudentAttendance.jsx';
+import StudentExamMarks from './studentRelated/StudentExamMarks.jsx';
+import ViewStudent from './studentRelated/ViewStudent.jsx';
+
+import AddNotice from './noticeRelated/AddNotice.jsx';
+import ShowNotices from './noticeRelated/ShowNotices.jsx';
+
+import ShowSubjects from './subjectRelated/ShowSubjects.jsx';
+import SubjectForm from './subjectRelated/SubjectForm.jsx';
+import ViewSubject from './subjectRelated/ViewSubject.jsx';
+
+import AddTeacher from './teacherRelated/AddTeacher.jsx';
+import ChooseClass from './teacherRelated/ChooseClass.jsx';
+import ChooseSubject from './teacherRelated/ChooseSubject.jsx';
+import ShowTeachers from './teacherRelated/ShowTeachers.jsx';
+import TeacherDetails from './teacherRelated/TeacherDetails.jsx';
+
+import AddClass from './classRelated/AddClass.jsx';
+import ClassDetails from './classRelated/ClassDetails.jsx';
+import ShowClasses from './classRelated/ShowClasses.jsx';
+import AccountMenu from '../../components/AccountMenu.jsx';
+
+
+// Define the AdminDashboard functional component.
+const AdminDashboard = () => {
+    // Use state to manage the state of the side drawer (whether it's open or closed).
+    const [open, setOpen] = useState(false);
+    // Define a function toggleDrawer to toggle the state of the drawer.
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
+
+    return (
+        // Render the main structure of the admin dashboard.
+        <>
+            <Box sx={{ display: 'flex' }}>
+            {/* Include a CssBaseline component to normalize CSS across browsers. */}
+                <CssBaseline />
+                {/* Render an AppBar component at the top with a menu icon to toggle the side drawer. */}
+                <AppBar open={open} position='absolute'>
+                    <Toolbar sx={{ pr: '24px' }}>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={toggleDrawer}
+                            sx={{
+                                marginRight: '36px',
+                                ...(open && { display: 'none' }),
+                            }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            component="h1"
+                            variant="h6"
+                            color="inherit"
+                            noWrap
+                            sx={{ flexGrow: 1 }}
+                        >
+                            Admin Dashboard
+                        </Typography>
+                        {/* top right stuff */}
+                        <AccountMenu />
+                    </Toolbar>
+                </AppBar>
+                 {/* Render a Drawer component for the side navigation bar. */}
+                <Drawer variant="permanent" open={open} sx={open ? styles.drawerStyled : styles.hideDrawer}>
+                    <Toolbar sx={styles.toolBarStyled}>
+                        <IconButton onClick={toggleDrawer}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </Toolbar>
+                    <Divider />
+                    <List component="nav">
+                        <SideBar />
+                    </List>
+                </Drawer>
+                <Box component="main" sx={styles.boxStyled}>
+                    <Toolbar />
+                    <Routes>
+                        <Route path="/" element={<AdminHomePage />} />
+                        <Route path='*' element={<Navigate to="/" />} />
+                        <Route path="/Admin/dashboard" element={<AdminHomePage />} />
+                        <Route path="/Admin/profile" element={<AdminProfile />} />
+                        
+
+
+                        {/* Notice */}
+                        <Route path="/Admin/addnotice" element={<AddNotice />} />
+                        <Route path="/Admin/notices" element={<ShowNotices />} />
+
+                        {/* Subject */}
+                        <Route path="/Admin/subjects" element={<ShowSubjects />} />
+                        <Route path="/Admin/subjects/subject/:classID/:subjectID" element={<ViewSubject />} />
+                        <Route path="/Admin/subjects/chooseclass" element={<ChooseClass situation="Subject" />} />
+
+                        <Route path="/Admin/addsubject/:id" element={<SubjectForm />} />
+                        <Route path="/Admin/class/subject/:classID/:subjectID" element={<ViewSubject />} />
+
+                        <Route path="/Admin/subject/student/attendance/:studentID/:subjectID" element={<StudentAttendance situation="Subject" />} />
+                        <Route path="/Admin/subject/student/marks/:studentID/:subjectID" element={<StudentExamMarks situation="Subject" />} />
+
+                        {/* Class */}
+                        <Route path="/Admin/addclass" element={<AddClass />} />
+                        <Route path="/Admin/classes" element={<ShowClasses />} />
+                        <Route path="/Admin/classes/class/:id" element={<ClassDetails />} />
+                        <Route path="/Admin/class/addstudents/:id" element={<AddStudent situation="Class" />} />
+
+                        {/* Student */}
+                        <Route path="/Admin/addstudents" element={<AddStudent situation="Student" />} />
+                        <Route path="/Admin/students" element={<ShowStudents />} />
+                        <Route path="/Admin/students/student/:id" element={<ViewStudent />} />
+                        <Route path="/Admin/students/student/attendance/:id" element={<StudentAttendance situation="Student" />} />
+                        <Route path="/Admin/students/student/marks/:id" element={<StudentExamMarks situation="Student" />} />
+
+                        {/* Teacher */}
+                        <Route path="/Admin/teachers" element={<ShowTeachers />} />
+                        <Route path="/Admin/teachers/teacher/:id" element={<TeacherDetails />} />
+                        <Route path="/Admin/teachers/chooseclass" element={<ChooseClass situation="Teacher" />} />
+                        <Route path="/Admin/teachers/choosesubject/:id" element={<ChooseSubject situation="Norm" />} />
+                        <Route path="/Admin/teachers/choosesubject/:classID/:teacherID" element={<ChooseSubject situation="Teacher" />} />
+                        <Route path="/Admin/teachers/addteacher/:id" element={<AddTeacher />} />
+
+                        <Route path="/logout" element={<Logout />} />
+                    </Routes>
+                </Box>
+            </Box>
+        </>
+    );
+}
+
+export default AdminDashboard
+
+const styles = {
+    boxStyled: {
+        backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    toolBarStyled: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        px: [1],
+    },
+    drawerStyled: {
+        display: "flex"
+    },
+    hideDrawer: {
+        display: 'flex',
+        '@media (max-width: 600px)': {
+            display: 'none',
+        },
+    },
+}
