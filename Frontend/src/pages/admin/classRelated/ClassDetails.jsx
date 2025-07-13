6ad4f1e43993a3ae97c08f5ddb65743bbc906cef
Harsh-wd <harshbansal.it26@gmail.com>
2025-07-13 22:53:40 +0530

    class related files  added

---
+++ Frontend/src/pages/admin/classRelated/ClassDetails.jsx
@@ -0,0 +1,288 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom'
+import { getAllTeachers } from '../../../redux/teacherRelated/teacherHandle.js';
+import { getClassDetails, getClassStudents, getSubjectList } from "../../../redux/sclassRelated/sclassHandle.js";
+import { deleteUser } from '../../../redux/userRelated/userHandle.js';
+import {
+    Box, Container, Typography, Tab, IconButton
+} from '@mui/material';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { resetSubjects } from "../../../redux/sclassRelated/sclassSlice.js";
+import { BlueButton} from "../../../components/buttonStyles.js";
+import TableTemplate from "../../../components/TableTemplate.jsx";
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import Popup from "../../../components/Popup.jsx";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+
+const ClassDetails = () => {
+    const params = useParams()
+    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    const { teachersList} = useSelector((state) => state.teacher);
+    const { subjectsList, sclassStudents, sclassDetails, loading, error, response, getresponse } = useSelector((state) => state.sclass);
+    const { currentUser } = useSelector((state) => state.user);
+    const classID = params.id
+
+    useEffect(() => {
+        dispatch(getClassDetails(classID, "Sclass"));
+        dispatch(getSubjectList(classID, "ClassSubjects"))
+        dispatch(getClassStudents(classID));
+        dispatch(getAllTeachers(currentUser._id));
+    }, [dispatch, classID,currentUser._id])
+    
+
+    if (error) {
+        console.log(error)
+    }
+
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const [showPopup, setShowPopup] = useState(false);
+    const [message, setMessage] = useState("");
+
+    const deleteHandler = (deleteID, address) => {
+        console.log(deleteID);
+        console.log(address);
+        setMessage("Deleted Successfully! ")
+        setShowPopup(true)
+        dispatch(deleteUser(deleteID, address))
+            .then(() => {
+                dispatch(getClassStudents(classID));
+                dispatch(resetSubjects())
+                dispatch(getSubjectList(classID, "ClassSubjects"))
+            })
+    }
+
+    const subjectColumns = [
+        { id: 'name', label: 'Subject Name', minWidth: 170 },
+        { id: 'code', label: 'Subject Code', minWidth: 100 },
+    ]
+
+    const subjectRows = subjectsList && subjectsList.length > 0 && subjectsList.map((subject) => {
+        return {
+            name: subject.subName,
+            code: subject.subCode,
+            id: subject._id,
+        };
+    })
+
+    const SubjectsButtonHaver = ({ row }) => {
+        return (
+            <>
+                <IconButton onClick={() => deleteHandler(row.id, "Subject")}>
+                    <DeleteIcon color="error" />
+                </IconButton>
+                <BlueButton
+                    variant="contained"
+                    onClick={() => {
+                        navigate(`/Admin/class/subject/${classID}/${row.id}`)
+                    }}
+                >
+                    View
+                </BlueButton >
+            </>
+        );
+    };
+
+    
+    const ClassSubjectsSection = () => {
+        return (
+            <>
+                {response ?
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: '16px' }}>
+                  
+                    </Box>
+                    :
+                    <>
+                        <Typography variant="h5" gutterBottom>
+                            Subjects List:
+                        </Typography>
+
+                        <TableTemplate buttonHaver={SubjectsButtonHaver} columns={subjectColumns} rows={subjectRows} />
+                        
+                    </>
+                }
+            </>
+        )
+    }
+
+    const studentColumns = [
+        { id: 'name', label: 'Name', minWidth: 170 },
+        { id: 'rollNum', label: 'Roll Number', minWidth: 100 },
+    ]
+
+    const studentRows = sclassStudents.map((student) => {
+        return {
+            name: student.name,
+            rollNum: student.rollNum,
+            id: student._id,
+        };
+    })
+
+    const StudentsButtonHaver = ({ row }) => {
+        return (
+            <>
+                <IconButton onClick={() => deleteHandler(row.id, "Student")}>
+                    <PersonRemoveIcon color="error" />
+                </IconButton>
+                <BlueButton
+                    variant="contained"
+                    onClick={() => navigate("/Admin/students/student/" + row.id)}
+                >
+                    View
+                </BlueButton>
+                
+            </>
+        );
+    };
+
+    
+    const ClassStudentsSection = () => {
+        return (
+            <>
+                {getresponse ? (
+                    <>
+                        <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: '16px' }}>
+                            
+                        </Box>
+                    </>
+                ) :
+                 (
+                    <>
+                        <Typography variant="h5" gutterBottom>
+                            Students List:
+                        </Typography>
+
+                        <TableTemplate buttonHaver={StudentsButtonHaver} columns={studentColumns} rows={studentRows} />
+                        
+                    </>
+                )}
+            </>
+        )
+    }
+    const columns = [
+        { id: 'name', label: 'Name', minWidth: 170 },
+        { id: 'teachSubject', label: 'Subject', minWidth: 100 },
+        { id: 'teachSclass', label: 'Class', minWidth: 170 },
+    ];
+
+    const rows = teachersList.map((teacher) => {
+        return {
+            name: teacher.name,
+            teachSubject: teacher.teachSubject?.subName || null,
+            teachSclass: teacher.teachSclass.sclassName,
+            teachSclassID: teacher.teachSclass._id,
+            id: teacher._id,
+        };
+    });
+
+    const TeacherButtonHaver = ({ row }) => {
+        return (
+            <>
+                <IconButton onClick={() => deleteHandler(row.id, "Teacher")}>
+                                            <PersonRemoveIcon color="error" />
+                                        </IconButton>
+                                        <BlueButton
+                                            variant="contained"
+                                            onClick={() => navigate("/Admin/teachers/teacher/" + row.id)}
+                                        >
+                                            View
+                                        </BlueButton>
+                
+            </>
+        );
+    };
+
+    const ClassTeachersSection = () => {
+        return (
+            <>
+                {response ?
+                    <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: '16px' }}>
+                        
+                    </Box>
+                    :
+                    <>
+                        <Typography variant="h5" gutterBottom>
+                            Teachers List:
+                        </Typography>
+
+                        <TableTemplate buttonHaver={TeacherButtonHaver} columns={columns} rows={rows} />
+                
+                    </>
+                }
+            </>
+        )
+    }
+
+    const ClassDetailsSection = () => {
+        const numberOfSubjects = subjectsList.length;
+        const numberOfStudents = sclassStudents.length;
+        
+
+        return (
+            <>
+                <Typography variant="h4" align="center" gutterBottom>
+                    Class Details
+                </Typography>
+                <Typography variant="h5" gutterBottom>
+                    This is Class {sclassDetails && sclassDetails.sclassName}
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                    Number of Subjects: {numberOfSubjects}
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                    Number of Students: {numberOfStudents}
+                </Typography>
+                
+            </>
+        );
+    }
+
+    return (
+        <>
+            {loading ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    <Box sx={{ width: '100%', typography: 'body1', }} >
+                        <TabContext value={value}>
+                            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                <TabList onChange={handleChange} sx={{ position: 'fixed', width: '100%', bgcolor: 'background.paper', zIndex: 1 }}>
+                                    <Tab label="Details" value="1" />
+                                    <Tab label="Subjects" value="2" />
+                                    <Tab label="Students" value="3" />
+                                    <Tab label="Teachers" value="4" />
+                                </TabList>
+                            </Box>
+                            <Container sx={{ marginTop: "3rem", marginBottom: "4rem" }}>
+                                <TabPanel value="1">
+                                    <ClassDetailsSection />
+                                </TabPanel>
+                                <TabPanel value="2">
+                                    <ClassSubjectsSection />
+                                </TabPanel>
+                                <TabPanel value="3">
+                                    <ClassStudentsSection />
+                                </TabPanel>
+                                <TabPanel value="4">
+                                    <ClassTeachersSection />
+                                </TabPanel>
+                            </Container>
+                        </TabContext>
+                    </Box>
+                </>
+            )}
+            <Popup message={message} setShowPopup={setShowPopup} showPopup={showPopup} />
+        </>
+    );
+};
+
+export default ClassDetails;
