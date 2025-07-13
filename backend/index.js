834a8cfbe0101ba123468fda2724c3b6fb611bfe
Harsh-wd <harshbansal.it26@gmail.com>
2025-07-13 23:12:43 +0530

    backend index added

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+const app = express()
+const Routes = require("./routes/route.js")
+const port =3000
+
+dotenv.config();
+
+app.use(express.json({ limit: '10mb' }))
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+app.use('/route', Routes);
+
+app.listen(3000, () => {
+    console.log("Server started at port no.3000");
+})
+
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// require('dotenv').config(); // Load environment variables
+
+// const app = express();
+// app.use(cors()); // Allow cross-origin requests
+// app.use(express.json());
+
+// // connect to MongoDB
+// mongoose.connect(process.env.MONGODB_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => {
+//   console.log('Connected to MongoDB');
+// }).catch((error) => {
+//   console.error('MongoDB connection error:', error);
+// });
+
+// app.listen(3000, () => console.log("Server started at port no.3000"));
