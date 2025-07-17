f26d23b5e186b2be325a10630ee5bb044efcb095
Harsh-wd <harshbansal.it26@gmail.com>
2025-07-17 13:21:02 +0530

    added

--- backend/models/sclassSchema.js
+++ backend/models/sclassSchema.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const Student = require('./studentSchema');
+const Subject = require('./subjectSchema');
+const Teacher = require('./teacherSchema');
 
 const sclassSchema = new mongoose.Schema({
     sclassName: {
@@ -10,5 +13,22 @@ const sclassSchema = new mongoose.Schema({
         ref: 'admin'
     },
 }, { timestamps: true });
+
+// Middleware to handle cascading deletes for related documents
+sclassSchema.pre('deleteMany', async function (next) {
+    try {
+        const classesToDelete = await this.model.find(this.getFilter());
+        const classIds = classesToDelete.map(c => c._id);
+
+        if (classIds.length > 0) {
+            await Student.deleteMany({ sclassName: { $in: classIds } });
+            await Subject.deleteMany({ sclassName: { $in: classIds } });
+            await Teacher.deleteMany({ teachSclass: { $in: classIds } });
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = mongoose.model("sclass", sclassSchema);
